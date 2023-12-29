f98d561089ab429f522cd1a630e1c5b4670791d7
Fernando Warno <ferwar@hotmail.com>
2023-12-29 00:45:34 -0300

    nodemon - mongoose - dotenv

---
+++ src/app.js
@@ -0,0 +1,61 @@
+const express = require('express');
+
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', false);
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
+const PORT = process.env.PORT || 3000;
+const CONNECTION = process.env.CONNECTION;
+
+const customers = [
+  {
+    name: 'Caleb',
+    industry: 'music',
+  },
+  {
+    name: 'John',
+    industry: 'networking',
+  },
+  {
+    name: 'Sal',
+    industry: 'sports medicine',
+  },
+];
+
+app.get('/', (req, res) => {
+  res.send('Welcome!');
+});
+
+app.get('/api/customers', (req, res) => {
+  res.send({ customers: customers });
+});
+
+app.post('/api/customers', (req, res) => {
+  console.log(req.body);
+  res.send(req.body);
+});
+
+app.post('/', (req, res) => {
+  res.send('This is a post request!');
+});
+
+const start = async () => {
+  try {
+    await mongoose.connect(CONNECTION);
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}.`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
